c4a64cd4c5b4297f23977f9f4bd3c0154bd56d1d
Abdullah <afmi5241@gmail.com>
2026-09-17 09:06:12 +0300

    Update greeting to eng

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld { 
     public static void main(String[] args) {
 
-         System.out.println("السلام عليكم World");
+         System.out.println(" asslam alaikum World");
                   System.out.println("feature World");
 
          }
